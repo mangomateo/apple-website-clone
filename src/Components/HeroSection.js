92965a68ec4d5d3ba0c3b30c1c67a5492e31b064
mangomateo <velarde.matthew@gmail.com>
2021-11-13 15:24:51 -0800

    hero section styling complete

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,10 +1,13 @@
+import RedApple from '../img/holiday-logo.png';
 
 const HeroSection = () => {
     return (
-        <div>
-            <h1>HERO SECTION</h1>
+        <div id="hero-section">
+            <img src={RedApple} alt="Gift Apple" id="red-apple"/>
+            <h1>Let the holidays be for everyone.</h1>
+            <button>Shop Gifts</button>
         </div>
     )
 }
 
-export default HeroSection
+export default HeroSection;
